64fd8ea3cc87129bf0ca55cebd7117b9f5cb46d0
Ashraful610 <ashalam610@gmail.com>
2023-04-05 00:14:04 +0600

    parsInt and parsFloat

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var age = 20;
-var height = 5;
+var height =" 5.4";
 var weight = 50;
-console.log(age / height)
+var result = parseFloat(height);
+// console.log(age / height)
+console.log(weight  + result)
